e36363e6fb5fc3590e61303e26aa1b71cc3a4099
nirvaan122 <90305956+nirvaan122@users.noreply.github.com>
2021-12-22 19:12:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d")
+greencar_width=75;
+greencar_height=100;
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+greencar_x=5;
+greencar_y=225;
+function add() {
+	background_imgTag=new Image();
+	background_imgTag.onload=uploadBackground;
+	background_imgTag.src =background_image;
+
+	greencar_imgTag=new Image();
+	greencar_imgTag.onload=uploadBackground;
+	greencar_imgTag.src =greencar_image;
+
+}
+
+function uploadBackground() {
+	//Define function ‘uploadBackground’
+
+}
+
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	//Define function to move the car upward
+}
+
+function down()
+{
+	//Define function to move the car downward
+}
+
+function left()
+{
+	//Define function to move the car left side
+}
+
+function right()
+{
+	//Define function to move the car right side
+}
